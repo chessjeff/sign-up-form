e4205d36f6943a45ab73e996fb85710e8c6f74e0
chessjeff <122488700+chessjeff@users.noreply.github.com>
2023-10-30 12:31:09 -0500

    Properly validates passwords match

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const form = document.querySelector('form');
 const password = document.getElementById('password')
 const confirmedPassword = document.getElementById('confirm-password')
 const passwordError = document.getElementById('error')
@@ -6,5 +5,7 @@ const passwordError = document.getElementById('error')
 function validatePassword() {
     if (password.value !== confirmedPassword.value) {
         passwordError.textContent = "Passwords do not match";
+    } else {
+        passwordError.textContent = '';
     }
 }
